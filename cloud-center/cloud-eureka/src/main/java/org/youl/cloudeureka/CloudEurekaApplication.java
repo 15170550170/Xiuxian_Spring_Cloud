f67027a3ac6e6b2034f839ad3b665d70b4b4575b
The shark <1397668813@qq.com>
2024-07-02 06:55:03 +0800

    the seconds commit

--- cloud-center/cloud-eureka/src/main/java/org/youl/cloudeureka/CloudEurekaApplication.java
+++ cloud-center/cloud-eureka/src/main/java/org/youl/cloudeureka/CloudEurekaApplication.java
@@ -2,7 +2,10 @@ package org.youl.cloudeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// 在启动类中添加注解 @EnableEurekaServer
+@EnableEurekaServer
 @SpringBootApplication
 public class CloudEurekaApplication {
 
