38b4b707844af6431def27bbaa58f3caa75d79eb
The shark <1397668813@qq.com>
2024-07-02 06:48:23 +0800

    first commit

---
+++ cloud-center/cloud-eureka/src/test/java/org/youl/cloudeureka/CloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youl.cloudeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
