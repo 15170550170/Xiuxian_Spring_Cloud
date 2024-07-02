cfea239b6c496437050c858b88c339c30836b21a
Zane <1397668813@qq.com>
2024-07-02 15:16:51 +0800

    提交uaa-provider服务

---
+++ uaa-provider/src/main/java/org/youl/uaaprovider/UaaProviderApplication.java
@@ -0,0 +1,13 @@
+package org.youl.uaaprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaaProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaaProviderApplication.class, args);
+	}
+
+}
---
+++ uaa-provider/src/test/java/org/youl/uaaprovider/UaaProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youl.uaaprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaaProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
