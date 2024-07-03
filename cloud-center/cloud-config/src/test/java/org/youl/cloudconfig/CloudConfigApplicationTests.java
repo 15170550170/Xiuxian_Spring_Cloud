e1135a1e0050e90e4b3c66ec953cd3c4ab902d03
Zane <1397668813@qq.com>
2024-07-03 18:06:48 +0800

    新增配置中心

---
+++ cloud-center/cloud-config/src/test/java/org/youl/cloudconfig/CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youl.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- uaa-provider/src/main/java/org/youl/uaaprovider/UaaProviderApplication.java
+++ uaa-provider/src/main/java/org/youl/uaaprovider/UaaProviderApplication.java
@@ -2,7 +2,9 @@ package org.youl.uaaprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UaaProviderApplication {
 
